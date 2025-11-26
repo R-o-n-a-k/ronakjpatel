9298d62d418df321cd452d38a4a252221caa169e
Ronak Patel <rroonnaakk7@gmail.com>
2025-11-27 00:02:14 +0530

    feat: initial commit

---
+++ src/consts.js
@@ -0,0 +1,45 @@
+export const homeMeta={
+    title: "Ronak Patel | Frontend Developer",
+    description:"Frontend developer with 3+ years’ experience building scalable apps, enhancing UX, and delivering performant web solutions."
+}
+
+export const projectsMeta={
+    title: "Projects | Ronak Patel",
+    description:"These are the project section."
+}
+
+export const careerMeta={
+    title: "Career | Ronak Patel",
+    description:"These are my work and education experience."
+}
+
+
+export const socials = [
+  { 
+    name: "github",
+    href: "https://github.com/R-o-n-a-k"
+  },
+  { 
+    name: "linkedin",
+    href: "https://www.linkedin.com/in/ronak-j-patel",
+  }
+];
+
+export const projectsDetail = [
+    {
+        title: "Reactique",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Recusandae debitis.",
+    },
+    {
+        title: "Somebodyfixit",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Recusandae debitis.",
+    },
+    {
+        title: "Portfolio",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Recusandae debitis.",
+    },
+    {
+        title: "FND",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Recusandae debitis.",
+    },
+];
